bfa318ebd9a93b831dffff92016155dd1d2ec665
Diego Tacconi <diego@tacconi.org>
2024-11-29 14:41:56 -0600

    Improve example file

--- RaspberryPi/HttpPostJson.py
+++ RaspberryPi/HttpPostJson.py
@@ -5,20 +5,24 @@ data = {
     "WaterSensor1": True,
     "WaterSensor2": False,
     "WaterSensor3": True,
-    "Temperature": 29.3, # Temperature in degrees Celsius
-    "Humidity": 70.0, # Relative humidity (RH) in percentage (%)
-    "Pressure": 1013.25, # Pressure in Millibars
+    "Temperature": 29.3,        # Temperature in degrees Celsius
+    "Humidity": 70.0,           # Relative humidity (RH) in percentage (%)
+    "Pressure": 1013.25,        # Pressure in Millibars
     "OrientationPitch": 0,
     "OrientationRoll": 0,
     "OrientationYaw": 0,
+    "GpsLatitude": 38.762305,   # GPS Latitude in Decimal Degrees
+    "GpsLongitude": -94.665118, # GPS Longitude in Decimal Degrees
+    "GpsAltitude": 359.6        # GPS Altitude in Meters
 }
  
 # Serializing json
 dataJson = json.dumps(data, indent=4)
  
-# Writing to sample.json
-with open("sample.json", "w") as outfile:
+# Writing to json file
+
+with open("RaspberryPi/example.json", "w") as outfile:
     outfile.write(dataJson)
 
-print(dataJson)
+# print(dataJson)
 
