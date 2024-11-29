46db1a2e836c12ca01a6f8241206e7484def5a52
Diego Tacconi <diego@tacconi.org>
2024-11-29 15:49:02 -0600

    Update GPS data

--- RaspberryPi/HttpPostJson.py
+++ RaspberryPi/HttpPostJson.py
@@ -5,15 +5,15 @@ data = {
     "WaterSensor1": True,
     "WaterSensor2": False,
     "WaterSensor3": True,
-    "Temperature": 29.3,        # Temperature in degrees Celsius
-    "Humidity": 70.0,           # Relative humidity (RH) in percentage (%)
-    "Pressure": 1013.25,        # Pressure in Millibars
+    "Temperature": 29.3,           # Temperature in degrees Celsius
+    "Humidity": 70.0,              # Relative humidity (RH) in percentage (%)
+    "Pressure": 1013.25,           # Pressure in Millibars
     "OrientationPitch": 0,
     "OrientationRoll": 0,
     "OrientationYaw": 0,
-    "GpsLatitude": 38.762305,   # GPS Latitude in Decimal Degrees
-    "GpsLongitude": -94.665118, # GPS Longitude in Decimal Degrees
-    "GpsAltitude": 359.6        # GPS Altitude in Meters
+    "GpsLatitude": 38.762393333,   # GPS Latitude in Decimal Degrees
+    "GpsLongitude": -94.665196667, # GPS Longitude in Decimal Degrees
+    "GpsAltitude": 322.9           # GPS Altitude in Meters
 }
  
 # Serializing json
---
+++ RaspberryPi/gps.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Remember to install 'pip3 install gpsd-py3'
+# Further information at https://github.com/MartijnBraam/gpsd-py3
+
+import gpsd
+
+# Connect to the local gpsd
+gpsd.connect()
+
+# Get gps position
+packet = gpsd.get_current()
+
+# Print data
+print()
+print(" Device: " + str(gpsd.device()))
+print(" Mode: " + str(packet.mode))
+print(" Satellites: " + str(packet.sats))
+
+if packet.mode >= 2:
+    print(" Latitude: " + str(packet.lat))
+    print(" Longitude: " + str(packet.lon))
+    print(" Track: " + str(packet.track))
+    print(" Horizontal Speed: " + str(packet.hspeed))
+    print(" Time: " + str(packet.time))
+    print(" Error: " + str(packet.error))
+else:
+    print(" Latitude: NOT AVAILABLE")
+    print(" Longitude: NOT AVAILABLE")
+    print(" Track: NOT AVAILABLE")
+    print(" Horizontal Speed: NOT AVAILABLE")
+    print(" Time: NOT AVAILABLE")
+    print(" Error: NOT AVAILABLE")
+
+if packet.mode >= 3:
+    print(" Altitude: " + str(packet.alt))
+    print(" Climb: " + str(packet.climb))
+else:
+    print(" Altitude: NOT AVAILABLE")
+    print(" Climb: NOT AVAILABLE")
+
+print()
