82eb1c14efb5635908b40313477d695080db8576
Diego Tacconi <diego@tacconi.org>
2024-12-09 18:46:10 -0600

    Add PostData every 2 seconds.

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -22,8 +22,8 @@ data = {
     "OrientationRoll": 0,
     "OrientationYaw": 0,
     "GpsStatus": 3,                # 0=no gps, 1=no fix, 2=2D fix, 3=3D fix
-    "GpsLatitude": 38.762393333,   # GPS Latitude in decimal degrees. Available when GpsStatus >= 2. Possible Values: -90.0 to 90.0
-    "GpsLongitude": -94.665196667, # GPS Longitude in decimal degrees. Available when GpsStatus >= 2. Possible Values: -180.0 to 180.0
+    "GpsLatitude": 32.89823,   # GPS Latitude in decimal degrees. Available when GpsStatus >= 2. Possible Values: -90.0 to 90.0
+    "GpsLongitude": -97.1115, # GPS Longitude in decimal degrees. Available when GpsStatus >= 2. Possible Values: -180.0 to 180.0
     "GpsAltitude": 322.9           # GPS Altitude in meters. Available when GpsStatus >= 3
 }
 
@@ -274,5 +274,5 @@ while running:
     # print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     UpdateData()
     PrintData()
-    # PostData()
-    time.sleep(1)
+    PostData()
+    time.sleep(2)
