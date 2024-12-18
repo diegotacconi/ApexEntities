6f3e8622407cab2060066bbd55444590d634f7de
Diego Tacconi <diego@tacconi.org>
2024-12-17 23:05:10 -0600

    Add logging example

---
+++ RaspberryPi/LogTest.py
@@ -0,0 +1,19 @@
+import logging
+
+# https://docs.python.org/3/library/logging.html#logging.basicConfig
+logging.basicConfig(
+    level=logging.DEBUG,
+    datefmt='%Y-%m-%d %H:%M:%S',
+    format='%(asctime)s.%(msecs)03d | %(levelname)-8s | %(message)s',
+    handlers=[
+        logging.FileHandler("/var/tmp/apexlog", mode='w'),
+        logging.StreamHandler()
+    ]
+)
+
+# https://docs.python.org/3/howto/logging.html
+logging.debug('debug message')
+logging.info('info message')
+logging.warning('warn message')
+logging.error('error message')
+logging.critical('critical message')
